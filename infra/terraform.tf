ae90dda794de7a8ca3ed8cb814569571d5988d37
rohan191102 <br.o.h.a.n.1.5.1.3@gmail.com>
2026-04-12 10:06:11 +0200

    terraform definition

---
+++ infra/terraform.tf
@@ -0,0 +1,13 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
